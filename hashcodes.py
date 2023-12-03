03018e46f7eba96f9cf0c0b6031a618397aa1cc3
David Kozinn <david@kozinn.com>
2023-12-03 21:57:26 +0000

    Cleanups & get input on command line

--- hashcodes.py
+++ hashcodes.py
@@ -1,30 +1,28 @@
+import sys
 
 NTOKENS = 2063592
-nprime = 47055833459
+NPRIME = 47055833459
 n8 = [0, 0, 0]
 nbits = [10, 12, 22]
 ihash = [0, 0, 0]
-callsign = ''
-c = ' 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ/'
+C = ' 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ/'
 
-#nargs = len(sys.argv) - 1
-# if nargs != 1:
-#     print('Usage:    hashcodes <callsign>')
-#     print('Examples: hashcodes PJ4/K1ABC')
-#     print('          hashcodes YW18FIFA')
-#     sys.exit()
+if len(sys.argv) != 2:
+    print(f'Usage:    {sys.argv[0]} <callsign>')
+    print(f'Examples: {sys.argv[0]} PJ4/K1ABC')
+    print(f'          {sys.argv[0]} YW18FIFA')
+    sys.exit(1)
 
-# callsign = sys.argv[1]
-callsign = "k2dbk  "  # K2DBK 584 2339 2396005
+callsign = sys.argv[1]
 callsign = callsign.strip().ljust(11).upper()
 
-for k in range(3):  # k = num of bits for hash
-    for i in range(11): # i = index into callsign
-        j = c.index(callsign[i]) # - 1 not needed, python array index start is 0
+for k in range(3):       # k = num of bits for hash
+    for i in range(11):  # i = index into callsign
+        j = C.index(callsign[i])
         n8[k] = 38 * n8[k] + j
-    ihash[k] = ((nprime * n8[k]) >> abs(nbits[k] - 64)) % 2**nbits[k]
+    ihash[k] = ((NPRIME * n8[k]) >> abs(nbits[k] - 64)) % 2**nbits[k]
 
-ih22_biased =(ihash[2] + NTOKENS) % 64
+ih22_biased = ihash[2] + NTOKENS
 print('Callsign         h10         h12         h22')
 print('-------------------------------------------')
 print(f'{callsign:<11} {ihash[0]:<9} {ihash[1]:<9} {ihash[2]:<9}')
